fc61ed11cff6a1cb87cdb87433e7b659fa91f206
Ahmed Samir <100007894590343@facebook.com>
2017-11-29 14:27:01 +0000

    t

---
+++ Meatier Shower/ResultViewController.h
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.h
+//  Meatier Shower
+//
+//  Created by Ahmed Samir on 11/29/17.
+//  Copyright © 2017 University of leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *highscoreLabel;
+
+@end
---
+++ Meatier Shower/ResultViewController.m
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.m
+//  Meatier Shower
+//
+//  Created by Ahmed Samir on 11/29/17.
+//  Copyright © 2017 University of leeds. All rights reserved.
+//
+
+#import "ResultViewController.h"
+
+@interface ResultViewController ()
+
+@end
+
+@implementation ResultViewController
+@synthesize scoreLabel,highscoreLabel;
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
